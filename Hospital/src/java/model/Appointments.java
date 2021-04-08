2c7e531c3ac8f5bc1e092ff0578a0184c995701c
Rakshitha <rakshithadilshan1@gmail.com>
2021-04-08 19:16:37 +0530

    added appoinments bean class

---
+++ Hospital/src/java/model/Appointments.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author PC
+ */
+public class Appointments {
+    private int sno;
+    private String fname;
+    private String lname;
+    private String docname;
+    private int mobileno;
+    private String date;
+    private String time;
+    private String deceased;
+    private String address;
+
+    /**
+     * @return the sno
+     */
+    public int getSno() {
+        return sno;
+    }
+
+    /**
+     * @param sno the sno to set
+     */
+    public void setSno(int sno) {
+        this.sno = sno;
+    }
+
+    /**
+     * @return the fname
+     */
+    public String getFname() {
+        return fname;
+    }
+
+    /**
+     * @param fname the fname to set
+     */
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    /**
+     * @return the lname
+     */
+    public String getLname() {
+        return lname;
+    }
+
+    /**
+     * @param lname the lname to set
+     */
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    /**
+     * @return the docname
+     */
+    public String getDocname() {
+        return docname;
+    }
+
+    /**
+     * @param docname the docname to set
+     */
+    public void setDocname(String docname) {
+        this.docname = docname;
+    }
+
+    /**
+     * @return the mobileno
+     */
+    public int getMobileno() {
+        return mobileno;
+    }
+
+    /**
+     * @param mobileno the mobileno to set
+     */
+    public void setMobileno(int mobileno) {
+        this.mobileno = mobileno;
+    }
+
+    /**
+     * @return the date
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the time
+     */
+    public String getTime() {
+        return time;
+    }
+
+    /**
+     * @param time the time to set
+     */
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    /**
+     * @return the deceased
+     */
+    public String getDeceased() {
+        return deceased;
+    }
+
+    /**
+     * @param deceased the deceased to set
+     */
+    public void setDeceased(String deceased) {
+        this.deceased = deceased;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+   
+    
+}
