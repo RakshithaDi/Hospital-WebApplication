17dca21d145a7d7bc71baa46d447a0fc9e0ee592
Rakshitha <rakshithadilshan1@gmail.com>
2021-05-11 14:34:08 +0530

    added db

--- Database/hospital.sql
+++ Database/hospital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2021 at 11:28 AM
+-- Generation Time: May 11, 2021 at 11:02 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.32
 
@@ -28,21 +28,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `appointment` (
-  `AptID` int(10) NOT NULL,
-  `LineNo` int(11) NOT NULL,
-  `PatientID` int(11) NOT NULL,
-  `DoctorID` int(11) NOT NULL,
-  `Date` date NOT NULL,
-  `Pharmacy` varchar(3) NOT NULL
+  `LineNo` int(10) NOT NULL,
+  `PatientID` int(10) NOT NULL,
+  `DoctorID` int(10) NOT NULL,
+  `Date` varchar(90) NOT NULL,
+  `Time` varchar(90) NOT NULL,
+  `Pharmacy` varchar(10) NOT NULL,
+  `AptID` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `appointment`
 --
 
-INSERT INTO `appointment` (`AptID`, `LineNo`, `PatientID`, `DoctorID`, `Date`, `Pharmacy`) VALUES
-(1, 0, 1, 1, '2021-04-13', 'Yes'),
-(2, 2, 2, 1, '2021-04-13', 'no');
+INSERT INTO `appointment` (`LineNo`, `PatientID`, `DoctorID`, `Date`, `Time`, `Pharmacy`, `AptID`) VALUES
+(1, 1, 2, '2021-05-16 ', '9:00 AM ', 'Yes', 1),
+(2, 2, 2, '2021-05-18 ', '8:00 PM ', 'No', 2);
 
 -- --------------------------------------------------------
 
@@ -52,13 +53,15 @@ INSERT INTO `appointment` (`AptID`, `LineNo`, `PatientID`, `DoctorID`, `Date`, `
 
 CREATE TABLE `doctor` (
   `DoctorID` int(11) NOT NULL,
-  `TypeID` int(11) NOT NULL,
+  `DoctorType` varchar(90) NOT NULL,
   `NIC` varchar(15) NOT NULL,
   `RegNo` varchar(25) NOT NULL,
   `FirstName` varchar(25) NOT NULL,
   `LastName` varchar(353) NOT NULL,
-  `PhoneNo` varchar(10) NOT NULL,
+  `Username` varchar(90) NOT NULL,
+  `PhoneNo` int(10) NOT NULL,
   `Email` varchar(89) NOT NULL,
+  `Password` varchar(255) NOT NULL,
   `Address` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -66,8 +69,36 @@ CREATE TABLE `doctor` (
 -- Dumping data for table `doctor`
 --
 
-INSERT INTO `doctor` (`DoctorID`, `TypeID`, `NIC`, `RegNo`, `FirstName`, `LastName`, `PhoneNo`, `Email`, `Address`) VALUES
-(1, 2, '123456789V', '34567891234', 'Chamara', 'Ratnayake', '1234567890', 'chamara.rat@gmail.com', 'Colombo');
+INSERT INTO `doctor` (`DoctorID`, `DoctorType`, `NIC`, `RegNo`, `FirstName`, `LastName`, `Username`, `PhoneNo`, `Email`, `Password`, `Address`) VALUES
+(1, 'Immunologist', '1998343259V', '0001', 'Kasun', 'Kalhara', 'Kasun', 713033789, 'kasunkalhara@gmail.com', '[15, -2, 26, -67, 26, 8, 33, 83, 83, -62, 51, -42, -32, 9, 97, 62, -107, -18, -60, 37, 56, 50, -89, 97, -81, 40, -1, 55, -84, 90, 21, 12]', 'No 22,Colombo'),
+(2, 'Anesthesiologist', '9809654346V', '0002', 'Ravindu', 'Jayathilake', 'Ravindu', 776547807, 'ravindujayathilake@gmail.com', '[-19, -18, 41, -8, -126, 84, 59, -107, 102, 32, -78, 109, 14, -32, -25, -23, 80, 57, -101, 28, 66, 34, -11, -34, 5, -32, 100, 37, -76, -55, -107, -23]', 'No 02, Rathnapura'),
+(3, 'Anesthesiologist', '20000010234', '0003', 'Hasiru ', 'Kalhara', 'Hasiru', 776547807, 'hasirunawodya@gmail.com', '[49, -118, -18, 63, -19, -116, -99, 4, 13, 53, -89, -4, 31, -89, 118, -5, 49, 48, 56, 51, -86, 45, -24, -123, 53, 77, -33, 61, 68, -40, -5, 105]', 'No 02, Rathnapura');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctordatetime`
+--
+
+CREATE TABLE `doctordatetime` (
+  `DoctorID` int(11) NOT NULL,
+  `Date` varchar(90) NOT NULL,
+  `Time` varchar(30) NOT NULL,
+  `ID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `doctordatetime`
+--
+
+INSERT INTO `doctordatetime` (`DoctorID`, `Date`, `Time`, `ID`) VALUES
+(2, '2021-05-16', '9:00 AM', 1),
+(2, '2021-05-17', '01:00 PM ', 2),
+(1, '2021-05-17', '12:00 PM', 3),
+(3, '2021-05-18', '3:00 PM', 4),
+(3, '2021-05-18', '8:00 PM', 5),
+(1, '2021-05-17', '12:13', 7),
+(2, '2021-05-20', '14:13', 8);
 
 -- --------------------------------------------------------
 
@@ -130,6 +161,17 @@ CREATE TABLE `feedback` (
   `Stars` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`FeedbackID`, `Text`, `Stars`) VALUES
+(1, 'Good Communication', 5),
+(2, 'Terrible Service', 1),
+(3, 'Good Service', 88),
+(4, 'Bad Service', 7),
+(5, 'Good Communication', 12);
+
 -- --------------------------------------------------------
 
 --
@@ -142,6 +184,7 @@ CREATE TABLE `patient` (
   `LastName` varchar(30) NOT NULL,
   `Email` varchar(89) NOT NULL,
   `PhoneNo` int(10) NOT NULL,
+  `Password` varchar(255) NOT NULL,
   `Address` varchar(250) NOT NULL,
   `DOB` date NOT NULL,
   `Gender` varchar(10) NOT NULL,
@@ -153,9 +196,9 @@ CREATE TABLE `patient` (
 -- Dumping data for table `patient`
 --
 
-INSERT INTO `patient` (`PatientID`, `FirstName`, `LastName`, `Email`, `PhoneNo`, `Address`, `DOB`, `Gender`, `BloodType`, `PEC`) VALUES
-(1, 'Nimal', 'Gamage', 'nim.gamage@gmail.com', 1236547895, 'Kandy', '1998-04-08', 'Male', 'A-', 'diabetes, allergic to paracetamol'),
-(2, 'Rakshitha', 'Dilshan', 'rakshithadilshan2@gmail.com', 77348481, 'nuwara eliya', '1991-04-12', 'Male', 'B+', 'diabetes, COPD');
+INSERT INTO `patient` (`PatientID`, `FirstName`, `LastName`, `Email`, `PhoneNo`, `Password`, `Address`, `DOB`, `Gender`, `BloodType`, `PEC`) VALUES
+(1, 'Nimal', 'Gamage', 'nimalgamage@gmail.com', 713033789, '[15, -2, 26, -67, 26, 8, 33, 83, 83, -62, 51, -42, -32, 9, 97, 62, -107, -18, -60, 37, 56, 50, -89, 97, -81, 40, -1, 55, -84, 90, 21, 12]', 'No 23, Gampaha', '1995-06-15', 'Male', 'A+', 'Diabetes'),
+(2, 'Dinusha', 'Lakmal', 'dinushalakmal1@gmail.com', 776547807, '[-19, -18, 41, -8, -126, 84, 59, -107, 102, 32, -78, 109, 14, -32, -25, -23, 80, 57, -101, 28, 66, 34, -11, -34, 5, -32, 100, 37, -76, -55, -107, -23]', 'No 22, Wajirapura', '1996-06-04', 'Male', 'A+', 'allergic to paracetamol');
 
 -- --------------------------------------------------------
 
@@ -164,10 +207,20 @@ INSERT INTO `patient` (`PatientID`, `FirstName`, `LastName`, `Email`, `PhoneNo`,
 --
 
 CREATE TABLE `pharmacist` (
-  `StaffID` varchar(10) NOT NULL,
+  `StaffID` int(10) NOT NULL,
   `Name` varchar(25) NOT NULL,
-  `Password` varchar(10) NOT NULL
+  `Username` varchar(90) NOT NULL,
+  `Password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pharmacist`
+--
+
+INSERT INTO `pharmacist` (`StaffID`, `Name`, `Username`, `Password`) VALUES
+(1, 'Pasindu Gimhana', 'Pasindu', '[15, -2, 26, -67, 26, 8, 33, 83, 83, -62, 51, -42, -32, 9, 97, 62, -107, -18, -60, 37, 56, 50, -89, 97, -81, 40, -1, 55, -84, 90, 21, 12]'),
+(2, 'Rakshitha Dilshan', 'Rakshitha', '[-19, -18, 41, -8, -126, 84, 59, -107, 102, 32, -78, 109, 14, -32, -25, -23, 80, 57, -101, 28, 66, 34, -11, -34, 5, -32, 100, 37, -76, -55, -107, -23]'),
+(3, 'Sadun Jayarathna', 'Sadun', '[49, -118, -18, 63, -19, -116, -99, 4, 13, 53, -89, -4, 31, -89, 118, -5, 49, 48, 56, 51, -86, 45, -24, -123, 53, 77, -33, 61, 68, -40, -5, 105]');
 
 -- --------------------------------------------------------
 
@@ -176,11 +229,20 @@ CREATE TABLE `pharmacist` (
 --
 
 CREATE TABLE `pharmacyorders` (
-  `OrderID` int(11) NOT NULL,
   `PrscID` int(11) NOT NULL,
-  `Ready` varchar(3) NOT NULL,
-  `Payment` varchar(3) NOT NULL
+  `Ready` varchar(30) NOT NULL,
+  `Price` varchar(50) NOT NULL,
+  `Note` varchar(90) NOT NULL,
+  `OrderID` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pharmacyorders`
+--
+
+INSERT INTO `pharmacyorders` (`PrscID`, `Ready`, `Price`, `Note`, `OrderID`) VALUES
+(1, 'Available', '500', 'Three times a day', 1),
+(2, 'Not Available', '', '', 2);
 
 -- --------------------------------------------------------
 
@@ -222,9 +284,7 @@ CREATE TABLE `prescription` (
 INSERT INTO `prescription` (`PrscID`, `PatientID`, `DoctorID`, `PrscList`, `Notes`, `Date`) VALUES
 (1, 1, 1, 'Metformin 5mg', 'Hypertension', '2021-04-26'),
 (2, 1, 1, 'Panadol 10mg', 'Fever', '2021-04-26'),
-(3, 2, 1, 'Fortamet, Glumetza', 'Hypertension', '2021-04-26'),
-(9, 2, 1, 'cvvcxvcxv', 'bvcb', '2021-04-26'),
-(10, 2, 1, 'gfgfgfgfgfg', 'gfgfgfgf', '2021-04-26');
+(3, 2, 1, 'Fortamet, Glumetza', 'Hypertension', '2021-04-26');
 
 -- --------------------------------------------------------
 
@@ -233,10 +293,37 @@ INSERT INTO `prescription` (`PrscID`, `PatientID`, `DoctorID`, `PrscList`, `Note
 --
 
 CREATE TABLE `receptionist` (
-  `StaffID` varchar(10) NOT NULL,
+  `StaffID` int(10) NOT NULL,
   `Name` varchar(25) NOT NULL,
-  `Password` varchar(10) NOT NULL
+  `Username` varchar(90) NOT NULL,
+  `Password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `receptionist`
+--
+
+INSERT INTO `receptionist` (`StaffID`, `Name`, `Username`, `Password`) VALUES
+(1, 'Suranga Lakmal', 'Suranga', '[15, -2, 26, -67, 26, 8, 33, 83, 83, -62, 51, -42, -32, 9, 97, 62, -107, -18, -60, 37, 56, 50, -89, 97, -81, 40, -1, 55, -84, 90, 21, 12]'),
+(2, 'Dilshan', 'Sanjeewa', '[-19, -18, 41, -8, -126, 84, 59, -107, 102, 32, -78, 109, 14, -32, -25, -23, 80, 57, -101, 28, 66, 34, -11, -34, 5, -32, 100, 37, -76, -55, -107, -23]');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `apid` varchar(90) NOT NULL,
+  `lineno` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`apid`, `lineno`) VALUES
+('213d', 'erere');
 
 -- --------------------------------------------------------
 
@@ -245,10 +332,18 @@ CREATE TABLE `receptionist` (
 --
 
 CREATE TABLE `webadmin` (
-  `StaffID` varchar(10) NOT NULL,
+  `StaffID` int(10) NOT NULL,
   `Name` varchar(25) NOT NULL,
-  `Password` varchar(10) NOT NULL
+  `Username` varchar(90) NOT NULL,
+  `Password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `webadmin`
+--
+
+INSERT INTO `webadmin` (`StaffID`, `Name`, `Username`, `Password`) VALUES
+(1, 'admin', 'admin', '[-116, 105, 118, -27, -75, 65, 4, 21, -67, -23, 8, -67, 77, -18, 21, -33, -79, 103, -87, -56, 115, -4, 75, -72, -88, 31, 111, 42, -76, 72, -87, 24]');
 
 -- --------------------------------------------------------
 
@@ -276,6 +371,12 @@ ALTER TABLE `doctor`
   ADD PRIMARY KEY (`DoctorID`);
 
 --
+-- Indexes for table `doctordatetime`
+--
+ALTER TABLE `doctordatetime`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `doctortype`
 --
 ALTER TABLE `doctortype`
@@ -294,6 +395,12 @@ ALTER TABLE `patient`
   ADD PRIMARY KEY (`PatientID`);
 
 --
+-- Indexes for table `pharmacist`
+--
+ALTER TABLE `pharmacist`
+  ADD PRIMARY KEY (`StaffID`);
+
+--
 -- Indexes for table `pharmacyorders`
 --
 ALTER TABLE `pharmacyorders`
@@ -306,6 +413,24 @@ ALTER TABLE `prescription`
   ADD PRIMARY KEY (`PrscID`);
 
 --
+-- Indexes for table `receptionist`
+--
+ALTER TABLE `receptionist`
+  ADD PRIMARY KEY (`StaffID`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`apid`);
+
+--
+-- Indexes for table `webadmin`
+--
+ALTER TABLE `webadmin`
+  ADD PRIMARY KEY (`StaffID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -313,13 +438,19 @@ ALTER TABLE `prescription`
 -- AUTO_INCREMENT for table `appointment`
 --
 ALTER TABLE `appointment`
-  MODIFY `AptID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `AptID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `doctor`
 --
 ALTER TABLE `doctor`
-  MODIFY `DoctorID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `DoctorID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `doctordatetime`
+--
+ALTER TABLE `doctordatetime`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `doctortype`
@@ -331,7 +462,7 @@ ALTER TABLE `doctortype`
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `FeedbackID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `FeedbackID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `patient`
@@ -340,16 +471,34 @@ ALTER TABLE `patient`
   MODIFY `PatientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `pharmacist`
+--
+ALTER TABLE `pharmacist`
+  MODIFY `StaffID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `pharmacyorders`
 --
 ALTER TABLE `pharmacyorders`
-  MODIFY `OrderID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `OrderID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `prescription`
 --
 ALTER TABLE `prescription`
-  MODIFY `PrscID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `PrscID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `receptionist`
+--
+ALTER TABLE `receptionist`
+  MODIFY `StaffID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `webadmin`
+--
+ALTER TABLE `webadmin`
+  MODIFY `StaffID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ Hospital/build/web/patient/chat/chat-patient.js
@@ -0,0 +1,140 @@
+function test(abc){
+    console.log(abc)
+   }
+
+var selected = '';
+var db = firebase.firestore();
+
+function initChat(email,name){
+    var docRef = db.collection("messages").doc(email);
+    docRef.get().then(function(doc) {
+        if (doc.exists) {
+            selected = email;
+            loadmessage(email);
+            
+        } else {
+            db.collection("messages").doc(email).set({
+                messageCount: 0,
+                name: name,
+                ltime: firebase.firestore.Timestamp.fromDate(new Date())
+            })  
+        } 
+    });  
+}
+
+function mcountListener(countCheck,email){
+    db.collection("messages").doc(email)
+    .onSnapshot(function(doc) {
+        var mcount = doc.data().messageCount;
+        //console.log("updated")
+        if(countCheck != mcount){
+            newmessage(mcount,email)
+        }
+        //console.log(mcount)
+    });
+}
+
+function loadmessage(email){
+
+    const chat_main = document.getElementById('chatcont')
+    chat_main.innerHTML = ""
+    selected = email;
+    //console.log(selected)
+    //console.log("hdcbsjdhcb"); 
+
+    db.collection("messages").doc(email).collection("messages").orderBy("order","asc").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            //console.log(doc.data());
+            var sender = doc.data().sender;
+
+            var textcont = document.createElement('div')
+            textcont.setAttribute("class","row d-xl-flex justify-content-xl-end")
+            textcont.setAttribute("style","margin-bottom: 5px;margin-top: 5px;")
+
+            var textcont2 = document.createElement('div')
+            textcont2.setAttribute("class","row d-xl-flex justify-content-xl-start")
+            textcont2.setAttribute("style","margin-bottom: 5px;margin-top: 5px;")
+                          
+            if(sender == "s"){
+                textcont.innerHTML = "<div class='col-auto d-xl-flex my-auto' style='background: #bebebe;color: rgb(255,255,255);border-radius: 38px;border-width: 1px;border-color: rgb(128,128,128);'><span style='font-family: Cabin, sans-serif;color: rgb(0,0,0);'>" + doc.data().text + "</span></div>";
+                chat_main.append(textcont);
+            }
+
+            else
+            {
+                textcont2.innerHTML = "<div class='col-auto d-xl-flex my-auto' style='background: #3840ff;color: rgb(255,255,255);border-radius: 38px;border-width: 1px;border-color: rgb(128,128,128);'><span style='font-family: Cabin, sans-serif;'>" + doc.data().text + "</span></div>";
+                chat_main.append(textcont2);
+            }
+
+            //textcont.innerText = doc.data().text;
+            //chat_main.append(textcont);
+            //console.log(sender)
+        });
+    });
+
+    var docRef = db.collection("messages").doc(email);
+    docRef.get().then(function(doc) {
+        var mcount = doc.data().messageCount;
+        countCheck = mcount;
+        //console.log(mcount)
+        mcountListener(countCheck,email)     
+    });       
+}
+
+function sendmessage(){
+
+    let message = document.getElementById("mtext").value
+    console.log(message)
+
+    var docRef = db.collection("messages").doc(selected);
+    docRef.get().then(function(doc) {
+        var mcount = doc.data().messageCount;
+        mcount = mcount + 1
+        
+        db.collection("messages").doc(selected).update({
+            messageCount: mcount,
+        })
+        console.log(selected)
+
+        db.collection("messages").doc(selected).collection("messages").doc().set({
+            sender: "f",
+            text: message,
+            order: mcount,
+            status: "unread",
+            timestamp: firebase.firestore.Timestamp.fromDate(new Date())
+        })
+    });
+    document.getElementById("mtext").value = ""        
+}
+
+function newmessage(mcount,email){
+    const chat_main = document.getElementById('chatcont')
+    //console.log(mcount)
+    db.collection("messages").doc(email).collection("messages").where("order", "==", mcount)
+    .get().then(function(querySnapshot){
+        querySnapshot.forEach(function(doc){    
+            //console.log(doc.data());
+            var sender = doc.data().sender;
+
+            var textcont = document.createElement('div')
+            textcont.setAttribute("class","row d-xl-flex justify-content-xl-end")
+            textcont.setAttribute("style","margin-bottom: 5px;margin-top: 5px;")
+
+            var textcont2 = document.createElement('div')
+            textcont2.setAttribute("class","row d-xl-flex justify-content-xl-start")
+            textcont2.setAttribute("style","margin-bottom: 5px;margin-top: 5px;")
+                          
+            if(sender == "s"){
+                textcont.innerHTML = "<div class='col-auto d-xl-flex my-auto' style='background: #bebebe;color: rgb(255,255,255);border-radius: 38px;border-width: 1px;border-color: rgb(128,128,128);'><span style='font-family: Cabin, sans-serif;color: rgb(0,0,0);'>" + doc.data().text + "</span></div>";
+                chat_main.append(textcont);
+            }
+
+            else
+            {
+                textcont2.innerHTML = "<div class='col-auto d-xl-flex my-auto' style='background: #3840ff;color: rgb(255,255,255);border-radius: 38px;border-width: 1px;border-color: rgb(128,128,128);'><span style='font-family: Cabin, sans-serif;'>" + doc.data().text + "</span></div>";
+                chat_main.append(textcont2);
+            }
+
+        });
+    });
+}
